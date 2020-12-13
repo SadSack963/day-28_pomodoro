95244af8a04d8ce13b425578cda991fba46b9cca
john <john@jpatmore.plus.com>
2020-12-13 20:51:26 +0000

    Final code

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
+LONG_BREAK_MIN = 1
+reps = 0
+stop = True
 
 
 # Print widget configuration
@@ -31,22 +33,58 @@ def print_config(widget, name):
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
+def reset_timer():
+    global reps, stop
+    stop = True
+    reps = 0
+    label_activity["text"] = "Timer"
+    label_activity["fg"] = GREEN
+    label_tick["text"] = ""
+    canvas.itemconfig(timer_text, text="00:00")
+    button_start["state"] = "active"
+
+
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-def start_timer():
-    count = 5 * 60
-    count_down(count)
+def start_timer(work=WORK_MIN, short=SHORT_BREAK_MIN, long=LONG_BREAK_MIN):
+    global reps, stop
+    stop = False
+    button_start["state"] = "disabled"
+    if reps == 7:
+        time = long * 60
+        label_activity["text"] = "Long Break"
+        label_activity["fg"] = RED
+    elif reps % 2 == 0:
+        time = work * 60
+        label_activity["text"] = "Work"
+        label_activity["fg"] = GREEN
+    else:
+        time = short * 60
+        label_activity["text"] = "Short Break"
+        label_activity["fg"] = PINK
+    count_down(time)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 def count_down(count):
-    minutes = count // 60  # Floor operator
-    seconds = count % 60  # Modulus operator
-    # global canvas
-    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds:02d}")
-    if count > 0:
-        window.after(1000, count_down, count - 1)
+    global reps, stop
+    if not stop:
+        minutes = count // 60  # Floor operator
+        seconds = count % 60  # Modulus operator
+        # global canvas
+        canvas.itemconfig(timer_text, text=f"{minutes}:{seconds:02}")
+        # Subtract 1 from count every 1000ms
+        if count > 0:
+            window.after(1000, count_down, count - 1)
+        else:
+            if reps % 2 == 0:
+                label_tick["text"] = label_tick["text"] + "✓"
+            reps += 1
+            if reps > 7:
+                reps = 0
+                label_tick["text"] = ""
+            start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -56,7 +94,7 @@ window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
 
-label_activity = tk.Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, "bold"))
+label_activity = tk.Label(text="Timer", width=11, bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, "bold"))
 
 canvas = tk.Canvas(width=202, height=225, bg=YELLOW, highlightthickness=0)  # Slightly larger than image
 image = tk.PhotoImage(file="tomato.png")
@@ -68,9 +106,9 @@ timer_text = canvas.create_text((102, 131), text="00:00", fill="white", font=(FO
 
 button_start = tk.Button(text="Start", padx=5, pady=5, font=(FONT_NAME, 12, "bold"), command=start_timer)
 
-button_reset = tk.Button(text="Reset", padx=5, pady=5, font=(FONT_NAME, 12, "bold"))
+button_reset = tk.Button(text="Reset", padx=5, pady=5, font=(FONT_NAME, 12, "bold"), command=reset_timer)
 
-label_tick = tk.Label(text='✓', bg=YELLOW, fg=GREEN, font=(FONT_NAME, 20, "bold"))
+label_tick = tk.Label(bg=YELLOW, fg=GREEN, font=(FONT_NAME, 20, "bold"))
 # label_tick = tk.Label(text=u'\u2713')
 # label_tick = tk.Label(text=u'\N{check mark}')
 
